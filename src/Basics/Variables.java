a0581fec315b876c69fa62cd106d391ad7b4d1ef
Akash R <akjai209@gmail.com>
2025-09-12 19:16:47 +0530

    Day 1: Variables, DataTypes and Operators

---
+++ src/Basics/Variables.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Variables {
+	
+	public static void main(String[] args) {	
+		int Date = 12092025;
+		char Gender = 'M';
+		String Name = "Akash.R";
+		
+		System.out.println("Today date was "+Date+". "+Name+" starting this Core Java Practice 4 weeks Challenge.");
+	}
+}
---
+++ src/Basics/practice/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+package Basics.practice;
+
+public class AddTwoNumbers {
+	
+	public static void main(String[] args) {
+		int num1 = 43;
+		int num2 = 34;
+		
+		int add = num1 + num2;
+		
+		System.out.println("The Addition of num1 and num2 is "+ add);
+	}
+
+}
