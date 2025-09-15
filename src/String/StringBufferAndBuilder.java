803acfc84e34b9c2a46f03296f3b5c8542d16464
Akash R <akjai209@gmail.com>
2025-09-15 20:23:28 +0530

    Day 4: Arrays (1D, 2D), String basics

---
+++ src/String/StringBufferAndBuilder.java
@@ -0,0 +1,10 @@
+package String;
+
+public class StringBufferAndBuilder {
+	
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("Akash");
+		sb.append(" Nandhini");
+	}
+
+}
