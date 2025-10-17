3094bdf1d0535a46b8b52c7a169894725db79a5e
Akash R <akjai209@gmail.com>
2025-10-17 19:58:01 +0530

    Day 19 : BufferedReader, BufferedWriter

---
+++ src/FileInputOutput/bufferedreader.java
@@ -0,0 +1,26 @@
+package FileInputOutput;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+class bufferedreader {
+	public static void main(String[] args) {
+		
+		try {
+			FileReader fr = new FileReader("Output.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String line = br.readLine();
+			
+		while (line != null) {
+			System.out.print(line);
+			line = br.readLine();
+		}
+			
+		br.close();
+		}
+		
+		catch (	Exception e) {
+			System.out.println("File not found");
+		}
+	}
+}
